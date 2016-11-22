8c9401446e7b110c0491d8988f9db5be8d1be36c
anamikagit <anamika.arya1990@gmail.com>
2016-11-22 03:26:34 -0800

    Adding button

---
+++ app/src/main/java/com/example/aarya/mydb/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.aarya.mydb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= (Button) findViewById(R.id.b1);
+    }
+}
