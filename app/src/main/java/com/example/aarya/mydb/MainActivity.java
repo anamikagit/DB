4bbdb9f788f9113ba7110f6a2479f8aba6a0b987
anamikagit <anamika.arya1990@gmail.com>
2016-11-22 04:03:18 -0800

    adding button 2

--- app/src/main/java/com/example/aarya/mydb/MainActivity.java
+++ app/src/main/java/com/example/aarya/mydb/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button button,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button= (Button) findViewById(R.id.b1);
+
+
     }
 }
